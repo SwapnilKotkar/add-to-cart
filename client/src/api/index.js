82612c372371d0a9e07d93d2d3425e8e3c9cbca9
swapnil <43784955+SwapnilKotkar@users.noreply.github.com>
2022-11-26 17:23:07 +0530

    updated

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,10 +1,6 @@
 import axios from 'axios'
-// import dotenv from 'dotenv'
 
-// dotenv.config();
-
-
-const API = axios.create({ baseURL: process.env.BASE_URL})
+const API = axios.create({ baseURL: process.env.AXIOS_BASE_URL})
 
 export const fetchProducts = (page) => API.get(`/products?page=${page}`);
 export const fetchProductsBySearch = (searchQuery) => API.get(`/products/search?searchQuery=${searchQuery.search || 'none'}&sku=${searchQuery.SKU}&tags=${searchQuery.tags}`);
