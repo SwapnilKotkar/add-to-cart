52b5a8470950203566fa49ce1267689ec5dbda22
swapnil <43784955+SwapnilKotkar@users.noreply.github.com>
2022-11-26 11:01:15 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import bodyParser from 'body-parser';
+import cors from 'cors'
 import dotenv from 'dotenv'
+
+import productRoutes from './routes/products.js'
 
 dotenv.config({path: './.env'});
 
 const app = express();
+
+//  "extended: true" -->  precises that the req.body object will contain values of any type instead of just strings.
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use('/products', productRoutes)
 
 mongoose.connect(process.env.MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(()=> app.listen(process.env.PORT, ()=> console.log(`Server is running on port ${process.env.PORT}`)))
